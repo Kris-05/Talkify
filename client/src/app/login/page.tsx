e66d8893834715a490fa35eec31c91eb07c973f2
Kris-05 <vjkrisna995@gmail.com>
2026-04-03 17:35:19 +0530

    Create auth context for user

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import Loading from '@/components/Loading';
+import { useAppData, user_service } from '@/context/appContext';
 import axios from 'axios';
 import { ArrowRight, Loader2, Mail } from 'lucide-react'
-import { useRouter } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
 import React, { useState } from 'react'
+import toast from 'react-hot-toast';
 
 const LoginPage = () => {
+
+  const { isAuth, loading: userLoading } = useAppData();
+
   const [email, setEmail] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const router = useRouter();
@@ -15,18 +21,21 @@ const LoginPage = () => {
     setLoading(true);
 
     try {
-      const { data } = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v2/login`, {
+      const { data } = await axios.post(`${user_service}/api/v2/login`, {
         email
       });
 
-      alert(data.message);
+      toast.success(data.message);
       router.push(`/verify?email=${email}`);
     } catch (err: any) {
-      alert(err.response.data.message);
+      toast.error(err.response.data.message);
     } finally {
       setLoading(false);
     }
   }
+
+  if (userLoading) return <Loading />
+  if (isAuth) return redirect("/chat");
 
   return (
     <div className='min-h-screen bg-gray-900 flex items-center justify-center p-4'>
---
+++ client/src/context/appContext.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
+
+export const user_service = process.env.NEXT_PUBLIC_USER_BACKEND;
+export const chat_service = process.env.NEXT_PUBLIC_CHAT_BACKEND;
+
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+}
+
+export interface Chat {
+  _id: string;
+  users: string[];
+  latestMessage: {
+    text: string;
+    sender: string;
+  };
+  createdAt: string;
+  updatedAt: string;
+  unseenCount?: number;
+}
+
+export interface Chats {
+  _id: string;
+  user: User;
+  chat: Chat;
+}
+
+interface AppContextType {
+  user: User | null;
+  loading: boolean;
+  isAuth: boolean;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined)
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isAuth, setIsAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchUser() {
+    try {
+      const token = Cookies.get("token");
+      const { data } = await axios.get(`${user_service}/api/v2/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      setUser(data);
+      setIsAuth(true);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  }
+
+  async function logoutUser() {
+    Cookies.remove("token");
+    setUser(null);
+    setIsAuth(false);
+    toast.success("User Logged out successfully");
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, [])
+
+  return <AppContext.Provider value={{ user, setUser, isAuth, setIsAuth, loading }}>
+    {children}
+    <Toaster/>
+  </AppContext.Provider>
+}
+
+export const useAppData = () : AppContextType => {
+  const context = useContext(AppContext)
+  if(!context) {
+    throw new Error("useappdata must be used within app provider");
+  }
+  return context;
+}
